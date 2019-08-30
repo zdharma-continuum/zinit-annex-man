d350d73f4e542f6a503a9cf821b93daeaee49424
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-08-30 22:37:54 +0200

    Working gems installation and the ronn-running hook

---
+++ z-p-zman.plugin.zsh
@@ -0,0 +1,35 @@
+# According to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+typeset -g ZMAN_REPO_DIR="${0:h}"
+typeset -g ZMAN_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/z-p-zman"
+
+unfunction zman
+autoload -Uz :zp-zman-handler zman
+
+zman-inst() {
+    print -P -- "\n%F{38}zman z-plugin: \%F{154}Installing %F{220}$1%F{154} gem locally in the z-p-zman directory...%f"
+}
+
+# Check if ronn dependencies are installed
+local zman_dep
+local -a farray
+for zman_dep in hpricot rdiscount mustache; do
+    farray=( $ZMAN_REPO_DIR/gems/$zman_dep-*(N[1]) )
+    [[ ! -d ${farray[1]} ]] && \
+        { zman-inst "$zman_dep"; gem install -i "$ZMAN_REPO_DIR" "$zman_dep"; }
+done
+
+unset -f zman-inst
+
+@zplg-register-z-plugin "z-p-zman" hook:atclone \
+    :zp-zman-handler \
+    :zp-zman-help-handler \
+    "zman''" # register a new ice-mod: zman''
+
+@zplg-register-z-plugin "z-p-zman" hook:atpull \
+    :zp-zman-handler \
+    :zp-zman-atpull-help-handler
